33cc8f045d69aed05d51bb67d4e8bf728ccf83b9
tomspost <tomc@3gmagic.com>
2016-12-26 11:43:21 +0000

    Added RSS XML feeds

--- api.js
+++ api.js
@@ -1,6 +1,14 @@
+var config = require("./config");
 var express = require('express');
 var app = express();
 var cors = require('cors');  // alow broswers to access from all sites
+
+var RSS = require('rss');
+
+/* lets create an rss feed */
+
+var feed = new RSS(config.feedOptions);
+
 
 app.use(cors());  
 app.use(express.static('public'))
@@ -8,8 +16,22 @@ app.use(express.static('public'))
 function api(rssItems,rssServers,tags){
 
 app.get('/test', function (req, res) {
-  res.send('Hello World!');
+  res.send('OMN server is up and running!!! <br/> with ', rssItems.length , ' RSS Items <br/> from ',rssServers.length);
 });
+
+
+app.get('/rss', function (req,res){
+  
+  var feed = new RSS(config.feedOptions);
+  
+  rssItems.forEach(function(item,i) {
+    feed.item(item);
+  });
+  res.type('application/rss+xml');
+  res.send(feed.xml({indent: false}));
+  
+});
+
 
 app.get('/everything', function(req, res) {
   
